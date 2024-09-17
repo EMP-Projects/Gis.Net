ff2122344e385444ea6808174050a0ff8b32cddf
gzileni <giuseppe.zileni@gmail.com>
2024-09-17 12:22:56 +0200

    added Attribute property

--- Gis.Net/Aws/AWSCore/DynamoDb/Models/AwsDynamoDbTableBase.cs
+++ Gis.Net/Aws/AWSCore/DynamoDb/Models/AwsDynamoDbTableBase.cs
@@ -7,18 +7,18 @@ namespace Gis.Net.Aws.AWSCore.DynamoDb.Models;
 public abstract class AwsDynamoDbTableBase : IAwsDynamoDbTableBase
 {
     /// <inheritdoc />
-    [DynamoDBHashKey]
+    [DynamoDBHashKey("Id")]
     public string Id { get; set; } = Guid.NewGuid().ToString();
     
     /// <inheritdoc />
-    [DynamoDBRangeKey]
+    [DynamoDBRangeKey("TimeStamp")]
     public string TimeStamp { get; set; } = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
 
     /// <inheritdoc />
-    [DynamoDBGlobalSecondaryIndexHashKey("key-index")]
+    [DynamoDBGlobalSecondaryIndexHashKey("Key-NextTimeStamp-Index", AttributeName = "Key")]
     public required string Key { get; set; }
 
     /// <inheritdoc />
-    [DynamoDBGlobalSecondaryIndexRangeKey("key-index")]
+    [DynamoDBGlobalSecondaryIndexRangeKey("Key-NextTimeStamp-Index", AttributeName = "NextTimeStamp")]
     public required string NextTimeStamp { get; set; }
 }
